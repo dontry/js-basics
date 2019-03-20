b45f43fbb0cec64a8e71adfd56015b189eb2f71c
dontry <mccoy018@gmail.com>
2019-03-20 13:24:17 +1100

    test(Generate a Serializable Value with Jest Snapshots):

---
+++ packages/testing/src/__tests__/super-heros.js
@@ -0,0 +1,7 @@
+import { getFlyingSuperHeros } from "../super-heros";
+
+test("returns super heros that can fly", () => {
+  const flyingHeros = getFlyingSuperHeros();
+  console.log(flyingHeros);
+  expect(flyingHeros).toMatchSnapshot();
+});
---
+++ packages/testing/src/components/CalculatorDisplay.js
@@ -0,0 +1,36 @@
+import React from "react";
+import PropTypes from "prop-types";
+import AutoScalingText from "./AutoScalingText";
+import { getFormattedValue } from "./utils";
+
+class CalculatorDisplay extends React.Component {
+  static propTypes = {
+    value: PropTypes.string.isRequired
+  };
+  render() {
+    const { value, ...props } = this.props;
+    const formattedValue = getFormattedValue(
+      value,
+      typeof window === "undefined" ? "en-US" : window.navigator.language
+    );
+
+    return (
+      <div
+        {...props}
+        css={{
+          color: "white",
+          background: "#1c191c",
+          lineHeight: "130px",
+          fontSize: "6em",
+          flex: "1"
+        }}
+      >
+        <span>
+          <AutoScalingText>{formattedValue}</AutoScalingText>
+        </span>
+      </div>
+    );
+  }
+}
+
+export default CalculatorDisplay;
