bbd5043dc3b726a1a4c5ced73b944b37b4bdb870
dontry <mccoy018@gmail.com>
2019-06-08 22:42:07 +1000

    (webpack) dev-server

--- packages/webpack/webpack.config.js
+++ packages/webpack/webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: "[name].css",
+      filename: "[name]_[contenthash8].css",
       chunkFilename: "[id].css"
     })
   ],
